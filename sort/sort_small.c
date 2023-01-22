79da8ea544cf173028e5aba786c075cba7b1c02a
Mewen Le Boudec <mle-boud@z2r12p6.42lyon.fr>
2023-01-22 20:55:00 +0100

    small adjustment in sort_in_b

--- sort/sort_small.c
+++ sort/sort_small.c
@@ -6,21 +6,18 @@
 /*   By: mle-boud <mle-boud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 08:53:40 by mle-boud          #+#    #+#             */
-/*   Updated: 2023/01/22 18:06:33 by mle-boud         ###   ########.fr       */
+/*   Updated: 2023/01/22 20:54:19 by mle-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
 static void	sort_in_b(t_pile *a, t_pile *b)
 {
 	t_stack	*tmp;
-	int		size;
 	int		x;
 
 	x = 0;
-	size = a->size;
 	tmp = a->head;
 	while (a->size > 3)
 	{
@@ -29,11 +26,10 @@ static void	sort_in_b(t_pile *a, t_pile *b)
 			pb(a, b);
 			x++;
 		}
-		if (find_location(a->head, x) < (a->size / 2))
+		if (find_location(tmp, x) < (a->size / 2))
 			sa(a);
 		else
 			ra(a);
-		size--;
 	}
 	if (check_if_sorted(b->head, b->size))
 		sb(b);
@@ -44,9 +40,12 @@ void	sort_small(t_pile *a, t_pile *b)
 	t_stack	*tmp;
 
 	replace_data_with_rank(a);
-	exit(EXIT_SUCCESS);
 	if (a->size > 3)
+	{
 		sort_in_b(a, b);
+	}
+	write(1, "sort in b done\n", 15);
+	exit(EXIT_SUCCESS);
 	tmp = a->head;
 	while (!check_if_sorted(a->head, a->size))
 	{
